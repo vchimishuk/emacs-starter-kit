3fb37e673bf61bb48acc5f209b0dfcf63120c61b
Viacheslav Chumushuk <voice@root.ua>
2012-01-21 15:45:13 +0200

    Renamed auto-complete-mode.el to auto-complete-init.el

--- viacheslav/auto-complete-init.el
+++ viacheslav/auto-complete-init.el
@@ -1,4 +1,4 @@
-;;; auto-complete-mode.el -- Emacs AutoComplete configuration.
+;;; auto-complete-init.el -- Emacs AutoComplete configuration.
 
 (add-to-list 'load-path "~/.emacs.d/viacheslav/auto-complete")
 (require 'auto-complete)
