5ec678a47b30dbfc606b85a3768217021e2a4065
Viacheslav Chimishuk <voice@root.ua>
2012-05-05 02:38:13 +0300

    auto-complete dictionaries loading added.

--- viacheslav/my-auto-complete-init.el
+++ viacheslav/my-auto-complete-init.el
@@ -3,5 +3,5 @@
 ;(add-to-list 'load-path "~/.emacs.d/viacheslav/auto-complete")
 (require 'auto-complete)
 (require 'auto-complete-config)
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/viacheslav/auto-complete/dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4/dict")
 (ac-config-default)
