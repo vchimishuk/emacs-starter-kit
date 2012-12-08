afdad3cda6ae2a9d5bf985ab9aad6663e10ac1f7
Viacheslav Chimishuk <voice@root.ua>
2012-12-08 17:17:18 +0200

    S-Return shortcut added. Some shortcuts fixed for Ukrainian keyboard layout enabled.

--- viacheslav/my-bindings.el
+++ viacheslav/my-bindings.el
@@ -9,10 +9,17 @@
 (global-set-key [(control w)] 'backward-kill-word)
 (global-set-key "\C-ck" 'kill-region)
 (global-set-key "\C-cc" 'comment-or-uncomment-region)
+(global-set-key (kbd "<S-return>") (kbd "C-e C-j"))
 ; (global-set-key "\C-[" 'dabbrev-expand)
 ;; Windows resizing.
 (global-set-key (kbd "C-C <left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-C <right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-C <up>") 'shrink-window)
 (global-set-key (kbd "C-C <down>") 'enlarge-window)
-
+;; Keyboard hothey binginds for Runssian and Ukrainian input language
+;; mode fix.
+(define-key function-key-map [(control ?т)] [(control ?n)])
+(define-key function-key-map [(control ?з)] [(control ?p)])
+(define-key function-key-map [(control ?а)] [(control ?f)])
+(define-key function-key-map [(control ?и)] [(control ?b)])
+(define-key function-key-map [(control ?в)] [(control ?d)])
