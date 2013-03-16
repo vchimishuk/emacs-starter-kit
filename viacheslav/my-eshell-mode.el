62118022f6db5065e1762435274e92d325a510f2
Viacheslav Chimishuk <voice@root.ua>
2013-03-17 00:08:05 +0200

    eshell prompt customization.

--- viacheslav/my-eshell-mode.el
+++ viacheslav/my-eshell-mode.el
@@ -6,7 +6,26 @@
   (let ((inhibit-read-only t))
     (erase-buffer)))
 
+(setq eshell-highlight-prompt nil)
+(setq eshell-prompt-function
+      (lambda ()
+        (let ((blue '(:foreground "#8ac6f2"))
+              (red '(:foreground "#e5786d")))
+          (concat (propertize "[" 'face blue)
+                  (propertize (concat user-login-name "@" system-name " ") 'face red)
+                  (propertize (file-name-nondirectory (eshell/pwd)) 'face blue)
+                  (propertize (if (= (user-uid) 0) "] #" "] $") 'face blue)
+                  (propertize " " 'face 'default)))))
+
 (defun my-eshell-mode-hook ()
   (local-set-key (kbd "C-l") 'eshell/clear))
 
 (add-hook 'eshell-mode-hook 'my-eshell-mode-hook)
+
+;; (custom-set-faces
+;;  ;; '(eshell-ls-archive ((((class color) (background dark)) (:foreground "salmon"))))
+;;  ;; '(eshell-ls-directory ((((class color) (background dark)) (:foreground "Skyblue"))))
+;;  ;; '(eshell-ls-executable ((((class color) (background dark)) (:foreground "palegreen"))))
+;;  ;; '(eshell-ls-missing ((((class color) (background dark)) (:foreground "tomato"))))
+;;  ;; '(eshell-ls-symlink ((((class color) (background dark)) (:foreground "Cyan"))))
+;;  '(eshell-prompt ((t (:foreground "red")))))
