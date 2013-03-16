560788a1903d702be439ce606a00a1f81bbfd236
Viacheslav Chimishuk <voice@root.ua>
2013-03-17 00:09:57 +0200

    Magit color theme changes.

---
+++ viacheslav/my-magit-mode-init.el
@@ -0,0 +1,11 @@
+;;; my-magit-mode-init.el -- My magit-mode customizations.
+
+(defun my-magit-mode-hook ()
+  ; (when (not window-system)
+  ;  (set-face-background 'magit-item-highlight "black"))
+  (set-face-foreground 'magit-diff-add wombat-green)
+  (set-face-foreground 'magit-diff-del wombat-red-1)
+  (set-face-background 'magit-whitespace-warning-face wombat-red-1)
+  (set-face-background 'magit-item-highlight wombat-gray))
+
+(add-hook 'magit-mode-hook 'my-magit-mode-hook)
