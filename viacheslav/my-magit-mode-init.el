db1e9376ea7b397e95213b08e39e63468ec26a50
Viacheslav Chimishuk <voice@root.ua>
2013-03-26 23:03:14 +0200

    Fixed add and del lines background in magit-status window diffs.

--- viacheslav/my-magit-mode-init.el
+++ viacheslav/my-magit-mode-init.el
@@ -4,7 +4,9 @@
   ; (when (not window-system)
   ;  (set-face-background 'magit-item-highlight "black"))
   (set-face-foreground 'magit-diff-add wombat-green)
+  (set-face-background 'magit-diff-add wombat-bg)
   (set-face-foreground 'magit-diff-del wombat-red-1)
+  (set-face-background 'magit-diff-del wombat-bg)
   (set-face-background 'magit-whitespace-warning-face wombat-red-1)
   (set-face-background 'magit-item-highlight wombat-gray))
 
